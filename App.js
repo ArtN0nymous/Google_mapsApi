154a3d8bd838e945ceec0fdd319d89bb6280b640
ArtN0nymous <jr616934@gmail.com>
2023-01-04 14:49:22 -0700

    mueve los pines linea recta entre las dos direcci

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import Reac,{useState,useEffect} from 'react';
+import * as Location from 'expo-location';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView,{Marker,Polyline} from 'react-native-maps';
 export default function App() {
+  const [state,setState]=useState({
+    region: {
+      latitude: 40.574457,
+      longitude: -105.070450,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.04,
+    },
+    origen:{latitude:40.574457,longitude:-105.070450},
+    destino:{latitude:40.590000,longitude:-105.070450}
+  });
+  // function onRegionChange(region) {
+  //   setState({
+  //     region : region
+  //   });
+  // }
+  // useEffect(() => {
+  //   //return current location but it not work, isn't change the current map
+  //   console.log(state);
+  //   (async()=>{
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status != 'granted') {
+  //       console.log('Permission to access location was denied');
+  //       return;
+  //     }
+  //     let location = await Location.getCurrentPositionAsync({});
+  //     let region = {latitude:location.coords.latitude,longitude:location.coords.longitude,latitudeDelta:0.09,longitudeDelta:0.04};
+  //     setState({region:region})
+  //   })();
+  // }, []);
+  const setOrigen=async(location)=>{
+    setState({...state,origen:location});
+  }
+  const setDestination=async(location)=>{
+    setState({...state,destino:location});
+  }
   return (
     <View style={styles.container}>
-      <MapView style={styles.map_container}/>
+      <MapView style={styles.map_container} initialRegion={state.region} showsUserLocation={true}>
+        <Marker coordinate={state.origen} draggable onDragEnd={(location)=>setOrigen(location.nativeEvent.coordinate)}/>
+        <Marker coordinate={state.destino} draggable onDragEnd={(location)=>setDestination(location.nativeEvent.coordinate)}/>
+        <Polyline coordinates={[state.origen,state.destino]}/>
+      </MapView>
     </View>
   );
 }
@@ -18,6 +58,6 @@ const styles = StyleSheet.create({
   },
   map_container:{
     width:'100%',
-    height:'100%'
+    height:'90%'
   }
 });
